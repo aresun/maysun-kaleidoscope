8846e0c7fd1e1cc5648306510ae0c274b25ad01b
aresun <2014229503@qq.com>
2021-12-23 19:44:05 +0800

    add const transformer;

--- src/utils/transform.js
+++ src/utils/transform.js
@@ -11,6 +11,11 @@ const processor_toConnectedName = _.flowRight(
   _.map(_.toLower),
   to_array
 );
+const processor_toConstVariableName = _.flowRight(
+  _.join(`_`),
+  _.map(_.toUper), // todo:: check api name
+  to_array
+);
 const processor_toClassIdentifier = _.flowRight(
   _.join(``),
   _.map(_.capitalize),
@@ -31,4 +36,8 @@ export function toConnectedName(str) {
 
 export function toClassIdentifier(str) {
   return processor_toClassIdentifier(str);
+}
+
+export function toConstVariableName(str) {
+  return processor_toConstVariableName(str);
 }
